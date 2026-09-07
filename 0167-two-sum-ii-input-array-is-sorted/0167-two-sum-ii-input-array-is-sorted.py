4d081815316ae4578c92e8c298abd67140bc6543
vaishnaviuttarkar <166795382+vaishnaviuttarkar@users.noreply.github.com>
2026-09-07 18:52:52 +0530

    Time: 2 ms (83.76%), Space: 20.4 MB (95.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:        
-        seen = {}
-        for i,num in enumerate(numbers):
-            competent = target - num
-            if competent not in seen:
-                seen[num] = i+1
+        
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            t1 = numbers[left]+numbers[right] 
+            if t1>target:
+                right -= 1
+            elif t1<target:
+                left += 1
             else:
-                return [seen[competent],i+1]
+                return [left+1,right+1]
+        
